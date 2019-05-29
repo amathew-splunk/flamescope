deae706e6d821a991e86dcd18972c44d1f01929d
amathew <amathew@splunk.com>
2019-05-29 12:36:46 -0700

    Set current working directory so it can find assets

--- run.py
+++ run.py
@@ -2,12 +2,15 @@
 Start development web server.
 """
 
+import os
 import sys
 from app import APP
 
 def main(argv):
+    os.chdir('/opt/splunk/flamescope')
     """Start development web server."""
     APP.run(host=APP.config['HOST'], port=APP.config['PORT'], threaded=True)
+
 
 if __name__ == "__main__":
     main(sys.argv)
