1385683502385527539fc7b27e7d67f95db4327f
amathew <amathew@splunk.com>
2019-05-29 12:48:45 -0700

    Maybe not needed ?

--- run.py
+++ run.py
@@ -7,7 +7,6 @@ import sys
 from app import APP
 
 def main(argv):
-    os.chdir('/opt/splunk/flamescope')
     """Start development web server."""
     APP.run(host=APP.config['HOST'], port=APP.config['PORT'], threaded=True)
 
