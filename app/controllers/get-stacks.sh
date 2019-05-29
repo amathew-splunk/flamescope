7d9436c0674b85517a4c60cb7dcc98b7e6379253
amathew <amathew@splunk.com>
2019-05-29 14:32:19 -0700

    Ugly hack

--- app/controllers/get-stacks.sh
+++ app/controllers/get-stacks.sh
@@ -4,7 +4,7 @@ mkdir -p /opt/splunk/flamescope/profiles
 pid=$(ps aux | grep "\[process-runner\]" | awk '{print $2}')
 
 # record the profiles
-perf record -F 49 -p $pid --call-graph dwarf sleep 30s
+perf_4.9 record -F 49 -p $pid --call-graph dwarf sleep 30s
 
 # save to 
-perf script --header > /opt/splunk/flamescope/profiles/$1
+perf_4.9 script --header > /opt/splunk/flamescope/profiles/$1
