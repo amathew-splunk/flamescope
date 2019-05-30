ec1418de316f2c00ceefab073d8c48fefee1018b
amathew <amathew@splunk.com>
2019-05-29 18:51:09 -0700

    Again change

--- app/controllers/get-stacks.sh
+++ app/controllers/get-stacks.sh
@@ -1,9 +1,11 @@
 # Make sure profile directory exists and change to it
 mkdir -p $1
+mkdir -p $1/dump
 cd $1
 
 # get the pid of the process runner
 pid=$(ps aux | grep "\[process-runner\]" | awk '{print $2}')
 
 # record the profiles
-perf_4.9 record -F 49 -p $pid --call-graph dwarf sleep 30s | perf_4.9 script --header > $1/$2
+perf_4.9 record -F 99 -p $pid --call-graph dwarf sleep 30s -o $1/dump/$2
+perf_4.9 script -i $1/dump/$2 --header > $1/$2
