5451e96c271a8be5d5723244c952e21f2076c019
amathew <amathew@splunk.com>
2019-05-29 19:16:45 -0700

    Sigh

--- app/controllers/get-stacks.sh
+++ app/controllers/get-stacks.sh
@@ -7,5 +7,5 @@ cd $1
 pid=$(ps aux | grep "\[process-runner\]" | awk '{print $2}')
 
 # record the profiles
-perf_4.9 record -F 99 -p $pid --call-graph dwarf sleep 30s -o $1/dump/$2
+perf_4.9 record -F 99 -p $pid --call-graph dwarf -o $1/dump/$2 sleep 30s
 perf_4.9 script -i $1/dump/$2 --header > $1/$2
