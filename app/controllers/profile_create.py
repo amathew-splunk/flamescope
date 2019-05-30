6fdf5f3c2e2c8fce57545a168b9cf3790f0c101d
amathew <amathew@splunk.com>
2019-05-29 17:28:02 -0700

    Final path fix

--- app/controllers/profile_create.py
+++ app/controllers/profile_create.py
@@ -8,5 +8,5 @@ import time
 # get profile files
 def create_new_profile(capture_duration):
     f = hex(random.getrandbits(64))[2:-1] + "_" + str(time.time())
-    subprocess.call("./"+ config.BASE_DIR + "/controllers/get-stacks.sh " + config.PROFILE_DIR + " " + f + " &", shell=True)
+    subprocess.call( config.BASE_DIR + "/controllers/get-stacks.sh " + config.PROFILE_DIR + " " + f + " &", shell=True)
     return f 
