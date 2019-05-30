833c1376d9596226b51345bfdefb3a1947a01425
amathew <amathew@splunk.com>
2019-05-30 11:15:26 -0700

    Handle in progress and finished

--- app/controllers/get-stacks.sh
+++ app/controllers/get-stacks.sh
@@ -3,9 +3,15 @@ mkdir -p $1
 mkdir -p $1/dump
 cd $1
 
+# touch to mark in progress
+touch $1/dump/$2
+
 # get the pid of the process runner
 pid=$(ps aux | grep "\[process-runner\]" | awk '{print $2}')
 
 # record the profiles
 perf_4.9 record -F 99 -p $pid --call-graph dwarf -o $1/dump/$2 sleep 30s
 perf_4.9 script -i $1/dump/$2 --header > $1/$2
+
+# cleanup to show finished
+rm $1/dump/$2
